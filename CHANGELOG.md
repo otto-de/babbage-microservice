997775bf6cf4971a206485f7da4536cb40857125
mgeissen <matthias.geissendoerfer@otto.de>
2023-03-06 15:04:58 +0100

    next snapshot
    Co-authored-by: Matthias Geißendörfer <matthias.geissendoerfer@otto.de>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## 0.0.3-SNAPSHOT
+
 ## 0.0.2
 * _aws-paramstore:_ Fix: Add missing `@ConfigurationProperties` for AWS ParamStore Properties.
 
--- buildSrc/src/main/kotlin/babbage.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/babbage.conventions.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "de.otto.babbage"
-version = "0.0.2"
+version = "0.0.3-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
