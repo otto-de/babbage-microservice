045e320abd8789d0d23067f18731e97bc8e9badf
mgeissen <matthias.geissendoerfer@otto.de>
2023-03-06 14:30:01 +0100

    add annotation for ConfigurationProperty to fix loading of param store properties
    Co-authored-by: Jonas Keegan <Jonas.Keegan@otto.de>
    Co-authored-by: Matthias Geißendörfer <matthias.geissendoerfer@otto.de>

--- aws-paramstore/src/main/kotlin/de/otto/babbage/aws/paramstore/ParamStoreProperties.kt
+++ aws-paramstore/src/main/kotlin/de/otto/babbage/aws/paramstore/ParamStoreProperties.kt
@@ -1,5 +1,8 @@
 package de.otto.babbage.aws.paramstore
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "babbage.aws.paramstore")
 class ParamStoreProperties {
     var path: String = ""
     var isAddWithLowestPrecedence: Boolean = false
