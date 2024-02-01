2e0b81a4470af6e80cb7c0201bdd542a16b4fbfc
mgeissen <matthias.geissendoerfer@otto.de>
2024-02-01 15:22:25 +0100

    upgrade dependencies (including spring boot 3.2.2)
    Co-authored-by: Matthias Geißendörfer <matthias.geissendoerfer@otto.de>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,10 +7,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")              // kotlin("jvm")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.10")                    // kotlin("plugin.spring")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.0")      // id("org.springframework.boot")
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.0")           // id("io.spring.dependency-management")
-    implementation("com.adarshr:gradle-test-logger-plugin:3.2.0")                   // id("com.adarshr.test-logger")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")       // id("io.gitlab.arturbosch.detekt")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")              // kotlin("jvm")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.22")                    // kotlin("plugin.spring")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.2")      // id("org.springframework.boot")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.4")           // id("io.spring.dependency-management")
+    implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")                   // id("com.adarshr.test-logger")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.5")       // id("io.gitlab.arturbosch.detekt")
 }
